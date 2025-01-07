9409227776edb577c5bbbeb6a21bc53095b04966
Scott Sproule <scott.sproule@ficonab.com>
2025-01-06 23:05:58 -0800

    maybe fixed tue to changes in atr

--- lib/weather_tracker_web/controllers/weather_conditions_controller.ex
+++ lib/weather_tracker_web/controllers/weather_conditions_controller.ex
@@ -12,7 +12,7 @@ defmodule WeatherTrackerWeb.WeatherConditionsController do
     nparams=params
     nparams=if params["temperature_c"] == nil do
       Logger.warning("Nulls to temperature. check")
-      Map.merge(params, %{altitude_m: 0.0, pressure_pa: 0.0, temperature_c: 0.0, humidity_rh: 0.5})
+      Map.merge(params, %{"altitude_m" => 0.0, "pressure_pa" => 0.0, "temperature_c" => 0.0, "humidity_rh" => 0.5})
     end
     Logger.warning("controller.create: params before create entry #{inspect(nparams)}")
     case WeatherConditions.create_entry(nparams) do
