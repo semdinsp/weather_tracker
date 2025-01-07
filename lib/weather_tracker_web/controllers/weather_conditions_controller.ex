c1d867f0eaff13ba9342c181fb9635add4710506
Scott Sproule <scott.sproule@ficonab.com>
2025-01-06 21:08:41 -0800

    update for paramter settings

--- lib/weather_tracker_web/controllers/weather_conditions_controller.ex
+++ lib/weather_tracker_web/controllers/weather_conditions_controller.ex
@@ -10,15 +10,14 @@ defmodule WeatherTrackerWeb.WeatherConditionsController do
 
   def create(conn, params) do
     params=if params["temperature_c"] == nil do
-      Logger.warning("Nulls to temperature.  need to fix this propertly")
+      Logger.warning("Nulls to temperature. check")
       Map.merge(params, %{altitude_m: 0, pressure_pa: 0, temperature_c: 0, humidity_rh: 0.5})
     end
+    Logger.warning("params before create entry #{inspect(params)}")
     case WeatherConditions.create_entry(params) do
       {:ok, weather_condition = %WeatherCondition{}} ->
         #Logger.debug("Successfully created a weather condition entry")
-        Logger.warning("success: inbound api call -  #{inspect(weather_condition )}")
-
-
+        Logger.debug("success: inbound api call -  #{inspect(weather_condition )}")
         conn
         |> put_status(:created)
         |> json(weather_condition)
