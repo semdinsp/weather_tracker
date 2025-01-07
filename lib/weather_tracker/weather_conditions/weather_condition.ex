48525f5328ce0a107da65a7ef78b69e47c26a9df
Scott Sproule <scott.sproule@ficonab.com>
2025-01-06 22:54:10 -0800

    added flags to start of create_changeset

--- lib/weather_tracker/weather_conditions/weather_condition.ex
+++ lib/weather_tracker/weather_conditions/weather_condition.ex
@@ -46,7 +46,7 @@ defmodule WeatherTracker.WeatherConditions.WeatherCondition do
   #request_id=GBhREGAmxoCtOnwAAAIh [warning] params before create entry %{:temperature_c => 0, :pressure_pa => 0, :humidity_rh => 0.5, :altitude_m => 0, "hostname" => "nerves-8c5a", "light_lumens" => 6.2208, "serial_number" => "8c5a", "voc_index" => 133}
 
   def create_changeset(weather_condition = %__MODULE__{}, attrs) do
-    Logger.warning("changeset: params before create changeset attrs: #{inspect(attrs)}")
+    Logger.warning("create_changeset: --->entry attrs: #{inspect(attrs)}")
     timestamp =
       NaiveDateTime.utc_now()
       |> NaiveDateTime.truncate(:second)
@@ -56,7 +56,7 @@ defmodule WeatherTracker.WeatherConditions.WeatherCondition do
     |> validate_required(@allowed_fields)
     |> put_change(:timestamp, timestamp)
 
-    Logger.warning("changeset: params after create changeset result: #{inspect(result)}")
+    Logger.warning("changeset: <--- exiting create changeset result: #{inspect(result)}")
     result
     # was
     #|> cast(attrs, @allowed_fields)
