089c66504bf378199897b382f2cb1daf6c6d5c63
Scott Sproule <scott.sproule@ficonab.com>
2025-01-28 22:02:57 -0800

    ee database url

--- config/runtime.exs
+++ config/runtime.exs
@@ -29,7 +29,7 @@ if config_env() == :prod do
         environment variable DATABASE_URL is missing.
         For example: ecto://USER:PASS@HOST/DATABASE
         """
- # IO.puts "SCOTT DB URL"
+ IO.puts "SCOTT DB URL #{inspect(database_url)}"
  # IO.inspect database_url hidr for esecurity
 
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
